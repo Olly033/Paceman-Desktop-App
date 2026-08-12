d789f071d707911f685b4ff9fc80d8364b30d2c3
Oliver Volkov <oliver.volkov@tptlive.ee>
2026-08-12 20:08:05 +0300

    revert: remove auto-update feature

--- preload.js
+++ preload.js
@@ -1,11 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('pacemanAPI', {
-  openExternal: (url) => ipcRenderer.invoke('open-external', url),
-  onUpdateStatus: (callback) => {
-    const handler = (event, data) => callback(data);
-    ipcRenderer.on('update-status', handler);
-    return () => ipcRenderer.removeListener('update-status', handler);
-  },
-  installUpdate: () => ipcRenderer.invoke('install-update')
+  openExternal: (url) => ipcRenderer.invoke('open-external', url)
 });
