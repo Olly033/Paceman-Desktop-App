a9e29201290b9a7a18ac71b20b7424771699f9d6
Oliver Volkov <oliver.volkov@tptlive.ee>
2026-08-22 08:26:10 +0300

    feat: add 20s buffer to completed run VOD downloads

--- renderer/app.js
+++ renderer/app.js
@@ -2717,7 +2717,7 @@
           let startTime = vodOffset || 0;
           let endTime = null;
           if (finish) {
-            endTime = startTime + finish / 1000;
+            endTime = startTime + finish / 1000 + 20;
           } else {
             const furthest = furthestIndex(runData);
             if (furthest.time > 0) {
